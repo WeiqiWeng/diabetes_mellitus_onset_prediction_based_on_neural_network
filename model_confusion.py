4c24f921f0f9302923f3ad10a1f4e5105424e726
Yue Lyu <lyu.yue1992@gmail.com>
2016-12-02 23:34:03 -0500

    fix bug

--- model_confusion.py
+++ model_confusion.py
@@ -4,12 +4,31 @@ import pandas as pd
 from sklearn.decomposition import PCA
 from sklearn.neural_network import MLPClassifier
 import matplotlib.pyplot as plt
-from sknn import ae, mlp
+
+def compute(nn, trn_X, trn_Y, tst_X, tst_Y):
+    nn.fit(trn_X, trn_Y)
+    result = nn.predict(tst_X)
+
+    confusion = [[0, 0], [0, 0]]
+    for i in range(len(tst_Y)):
+        if (tst_Y[i] > 0.0 and result[i] > 0.0):
+            confusion[1][1] += 1
+        elif (tst_Y[i] > 0.0 and result[i] < 1.0):
+            confusion[1][0] += 1
+        elif (tst_Y[i] < 1.0 and result[i] > 0.0):
+            confusion[0][1] += 1
+        elif (tst_Y[i] < 1.0 and result[i] < 1.0):
+            confusion[0][0] += 1
+
+    return confusion[1][1] / len(tst_Y), confusion[0][0] / len(tst_Y)
 
 # Train model (PCA)
-df = np.array(pd.read_pickle('../data/diabetes_train.pickle'))
-X = df[:, 0:-1]
-Y = df[:, -1]
+df = pd.read_pickle('../data/diabetes_train.pickle')
+df_validation = pd.read_pickle('../data/diabetes_validation.pickle')
+df_train = np.array(pd.concat([df, df_validation]))
+
+X = df_train[:, 0:-1]
+trn_Y = df_train[:, -1]
 
 pca_config = pickle.load(open( "../data/pca_config.pickle", "rb" ))
 pca_config = pca_config[5]
@@ -25,28 +44,38 @@ pca = PCA(n_components=n_component)
 pca_X = pca.fit_transform(X)
 
 nn = MLPClassifier(solver='lbfgs', max_iter=1000, alpha=weight_decay, hidden_layer_sizes=(hidden_layer_size))
-nn.fit(pca_X, Y)
 
-# Predict
+
 testFile = np.array(pd.read_pickle('../data/diabetes_test.pickle'))
 X = testFile[:, 0:-1]
-Y = testFile[:, -1]
+tst_Y = testFile[:, -1]
 
 tst_X = pca.transform(X)
-result = nn.predict(tst_X)
+sen_list = []
+spec_list = []
+i_list = []
+maxi = 100
+for i in range(maxi - 5):
+    mean_sen, mean_spec = 0.0, 0.0
+    print(i)
+    i_list.append(i + 5)
+    for j in range(i + 5):
+        sen, spec = compute(nn, pca_X, trn_Y, tst_X, tst_Y)
+        mean_sen += sen
+        mean_spec += spec
+    sen_list.append(mean_sen / (i + 5))
+    spec_list.append(mean_spec / (i + 5))
 
 
-confusion = [[0, 0], [0, 0]]
-for i in range(len(Y)):
-    if (Y[i] > 0.0 and result[i] > 0.0):
-        confusion[1][1] += 1
-    elif (Y[i] > 0.0 and result[i] < 1.0):
-        confusion[1][0] += 1
-    elif (Y[i] < 1.0 and result[i] > 0.0):
-        confusion[0][1] += 1
-    elif (Y[i] < 1.0 and result[i] < 1.0):
-        confusion[0][0] += 1
-
-print(confusion)
-print(Y)
-print(result)
+plt.style.use('ggplot')
+plt.xlim(0, maxi)
+plt.plot(i_list, sen_list, 'ro')
+plt.plot(i_list, spec_list, 'bo')
+plt.ylim(0.2, 0.5)
+title = 'Mean Sensitivity (r) and specificity (b)'
+plt.title(title)
+plt.xlabel('epochs')
+plt.ylabel('mean')
+plt.legend(loc='best')
+plt.savefig('../pic/meanSens&Spec.png')
+plt.close()
